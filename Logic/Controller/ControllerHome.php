714a7375115ce33e79e5938bdf9f5866ca9e2278
mitivil <mitivili333@gmail.com>
2024-03-01 18:46:06 +0300

    Тест веб-хук 9

--- Logic/Controller/ControllerHome.php
+++ Logic/Controller/ControllerHome.php
@@ -13,18 +13,18 @@ class ControllerHome
     {
         // Загружаем модель.    
         $modelHome = Loader::model('ModelHome');
-
+   
 
         // Формируем 
         $data = [
             'header_file'   => VIEW_PATH . '/Common/header.php',
             'content_file'  => VIEW_PATH . '/Page/home.php',
             'footer_file'   => VIEW_PATH . '/Common/footer.php',    
-        ];  
+        ];    
 
         // Выводим данные на макет. 
         Loader::goView('index.php', $data);
-    }
+    }   
 
     public function apiRequestTest()
     {
